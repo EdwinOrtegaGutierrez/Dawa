e3b7590135434a44db2cc35a45b4272ddbb0652c
Eduardo Alvarado <[200~gatubelo48@gmail.com>
2023-04-07 17:26:19 -0600

    Se agrego agregaron las clases para trabajar con enteros, flotantes y booleanos

---
+++ Compiler/Compiler/Float.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		private bool flotantes(float num)
+		{
+            if (num.GetType == float) {
+            int i = 0;
+                while (i < num.length)
+                {
+                    if (num[i] == "int =")
+                    {
+                        return true;
+                    }
+                    i = i + 1;
+                }
+            }
+            else
+            {
+                Console.Write("El tipo de dato no es flotante");
+                return false;
+            }
+        }
+	}
+}
